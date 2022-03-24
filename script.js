f76e8e6b522555e36863f44827bea849941fa8b1
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-24 17:57:22 +0545

    Create simple hover effects

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container") // get div with class container
 
-function makeRows(rows, cols) {
-    container.style.setProperty("--grid-rows", rows)
-    container.style.setProperty("--grid-cols", cols)
-    for (c = 0; c < rows * cols; c++) {
-        let cell = document.createElement("div")
-        container.appendChild(cell).className = "box"
+function makeRows(
+    rows, // number of rows to be made
+    cols // number of columns to be made
+) {
+    container.style.setProperty("--grid-rows", rows) // sets the property of rows inside container class with rows entered
+    container.style.setProperty("--grid-cols", cols) // sets the property of columns inside container class with columns entered
+    for (
+        c = 0;
+        c < rows * cols; // number of times the loop should run ((Row * Column) times)
+        c++
+    ) {
+        let cell = document.createElement("div") // Create element div
+        container.appendChild(cell).className = "box" // Add created div to the containeer with class box
     }
 }
 
