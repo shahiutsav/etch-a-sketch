00e6bfd2f0651d0306c7169a129751fc7b7f9874
Utsav Shahi <shahiutsav180@gmail.com>
2022-03-24 17:50:30 +0545

    Configure the grid

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function makeRows(rows, cols) {
     container.style.setProperty("--grid-cols", cols)
     for (c = 0; c < rows * cols; c++) {
         let cell = document.createElement("div")
-        cell.innerText = c + 1
         container.appendChild(cell).className = "box"
     }
 }
